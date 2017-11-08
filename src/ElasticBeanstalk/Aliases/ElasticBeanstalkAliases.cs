5f41aae898e8531f617f4dd769bb46f43c8c8ae8
Mathieu Kempe <mathieukempe@gmail.com>
2017-11-08 17:08:35 +1100

    Fix issue with applicationName being string empty

--- src/ElasticBeanstalk/Aliases/ElasticBeanstalkAliases.cs
+++ src/ElasticBeanstalk/Aliases/ElasticBeanstalkAliases.cs
@@ -21,12 +21,13 @@ namespace Cake.AWS.ElasticBeanstalk
         [CakeAliasCategory("ElasticBeanstalk")]
         public static bool CreateApplicationVersion(this ICakeContext context, string applicationName, string description, string versionLabel, string s3Bucket, string s3Key, bool autoCreateApplication, ElasticBeanstalkSettings settings)
         {
-             return context.CreateManager().CreateApplicationVersion("", 
-                "", 
-                "", 
-                "", 
-                "",
-                true, settings);
+            var manager = context.CreateManager();
+            return manager.CreateApplicationVersion(applicationName,                    
+                description, 
+                versionLabel,
+                s3Bucket, 
+                s3Key, 
+                autoCreateApplication, settings);
         }
 
     }
